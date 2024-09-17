7a12215bd0d093a6090d2b89d67f2e8e4befd3df
spnch1 <dkulahin.edu@gmail.com>
2024-09-17 22:58:18 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,45 +6,50 @@
 
 int main(void) {
     unsigned char DP=0; //Decimal Places
-    double a=0, b=0, c=0; //Sides of the triangle - N
+    double a=0, b=0, c=0; //Sides of the triangle
     double P=0, p=0, S=0, ha=0, hb=0, hc=0, ma=0, mb=0, mc=0, ba=0, bb=0, bc=0; //Perimeter, half-perimeter, area, heights drawn to side N, medians drawn to side N, bisectors drawn to side N
     char choice = 0;
 
     printf("The program calculates the perimeter, area, heights, medians and bisectors of the given triangle.\n");
-    printf("Please enter values of the triangle's sides. The accepted range is [0.001, 1000].\n\n");
+    printf("Please enter values of the triangle's sides. The accepted range is [0.001, 1000].\n");
 
-    while (1) {
+    do {
+        a = b = c = 0;
+        DP = 0;
+        choice = 0;
+
         while (a + b <= c || a + c <= b || b + c <= a) {
-            printf("1st value:\n");
-            while (scanf("%lf", &a) != 1 || a < 0.001 || a > 1000) {
+            fflush(stdin);
+            printf("\n1st value:\n");
+            while (scanf(" %lf", &a) != 1 || a < 0.001 || a > 1000) {
                 printf("Invalid input. Please try again...\n");
                 fflush(stdin);
             }
 
-            printf("2nd value:\n");
-            while (scanf("%lf", &b) != 1 || b < 0.001 || b > 1000) {
+            printf("\n2nd value:\n");
+            while (scanf(" %lf", &b) != 1 || b < 0.001 || b > 1000) {
                 printf("Invalid input. Please try again...\n");
                 fflush(stdin);
             }
 
-            printf("3rd value:\n");
-            while (scanf("%lf", &c) != 1 || c < 0.001 || c > 1000) {
+            printf("\n3rd value:\n");
+            while (scanf(" %lf", &c) != 1 || c < 0.001 || c > 1000) {
                 printf("Invalid input. Please try again...\n");
                 fflush(stdin);
             }
 
             if (a + b <= c || a + c <= b || b + c <= a) {
-                printf("Invalid input. These sides do not form a triangle. Press press Enter to try again...\n");
+                printf("\nInvalid input. These sides do not form a triangle. Press press Enter to try again...\n");
                 fflush(stdin);
                 getch();
             }
         }
 
-        while (DP == 0) {
-            printf("Now enter the number of decimal points [0,12] for the results:\n");
-            while (scanf("%hhu", &DP) != 1) {
-                printf("Invalid input. Please try again...\n");
-            }
+        printf("\nNow enter the number of decimal points [0,12] for the results:\n");
+
+        while (scanf(" %hhu", &DP) != 1 || DP < 0 || DP > 12) {
+            printf("\nInvalid input. Please try again...\n");
+            fflush(stdin);
         }
 
         P = a+b+c;
@@ -72,26 +77,28 @@ int main(void) {
 
         fflush(stdin);
 
-        while (choice == 0) {
-            printf("\n\nRestart the program? [Y/N]\n");
+        printf("\n\nRestart the program? [Y/N]\n");
 
-            while (scanf("%c", &choice) != 1) {
-                printf("Please, only enter either Y or N.\n");
-                choice = tolower(choice);
-                if (choice == 'y' || choice == 'n') {
-                    break;
-                }
+        while (choice == 0) {
+            choice = getchar();
+            choice = tolower(choice);
+            if (choice == 'y' || choice == 'n') {
+                break;
             }
-        }
-        if (choice == 'y') {
+            printf("Invalid input. Please enter Y or N.\n");
             fflush(stdin);
-            clearScreen();
-            continue;
         }
+
         if (choice == 'n') {
             break;
         }
-    }
+
+        clearScreen();
+
+        printf("\n");
+        fflush(stdin);
+
+    } while (choice == 'y');
 
     return 0;
 }
